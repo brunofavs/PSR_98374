c7546260275f7c6795b6683dcdd5ad9daa2c6eb0
brunofavs <bruno.favs@ua.pt>
2022-09-25 23:00:38 +0100

    P1

---
+++ Ex1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# shebang line, tells the termninal which compiler to use
+
+def main():
+    print("Hello World !")
+
+    # write code here !!!
+
+if __name__ == "__main__":
+    main()
